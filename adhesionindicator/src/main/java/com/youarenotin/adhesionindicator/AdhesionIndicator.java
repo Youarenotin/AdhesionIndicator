a0ee168deea19e70942b7c8239110732ed6c25aa
lubo7525274 <lubo_wen@126.com>
2016-05-31 18:33:01 +0800

    1.1

--- adhesionindicator/src/main/java/com/youarenotin/adhesionindicator/AdhesionIndicator.java
+++ adhesionindicator/src/main/java/com/youarenotin/adhesionindicator/AdhesionIndicator.java
@@ -6,10 +6,13 @@ import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * 作者：lubo on 5/31 0031 15:14
@@ -28,6 +31,7 @@ public class AdhesionIndicator extends FrameLayout {
     private ViewPager viewPager;
     private Circle circleView;
     private LinearLayout container;
+    private ArrayList<TextView> tabs;
 
     public AdhesionIndicator(Context context) {
         super(context);
@@ -67,7 +71,30 @@ public class AdhesionIndicator extends FrameLayout {
     }
 
     private void setUpListener() {
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            // This method will be invoked when the current page is scrolled, either as part
+            // of a programmatically initiated smooth scroll or a user initiated touch scroll.
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
+            }
+            //This method will be invoked when a new page becomes selected. Animation is not
+            //necessarily complete.
+            @Override
+            public void onPageSelected(int position) {
+                for (TextView tv : tabs){
+                    tv.setTextColor(textColor);
+                }
+                tabs.get(position).setTextColor(selected_textColor);
+            }
+            //Called when the scroll state changes. Useful for discovering when the user
+            //begins dragging, when the pager is automatically settling to the current page,
+            //or when it is fully stopped/idle.
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     private void initView() {
@@ -92,14 +119,29 @@ public class AdhesionIndicator extends FrameLayout {
 
     private void addTabItem() {
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT,1.0f);
+        tabs = new ArrayList<>();
         for (int i =0 ; i<viewPager.getAdapter().getCount(); i ++){
             TextView tv = new TextView(getContext());
             if (viewPager.getAdapter().getPageTitle(i)!=null){
                 tv.setText(viewPager.getAdapter().getPageTitle(i));
             }
-            tv.setTextSize(TypedValue.COMPLEX_UNIT_SP,textSize);
+            tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize);
+            tv.setTextColor(textColor);
             tv.setGravity(Gravity.CENTER);
-            
+            if (textBgColor!=0){
+                tv.setBackgroundResource(textBgColor);
+            }
+            tv.setLayoutParams(params);
+            tv.setClickable(true);
+            final int finalI = i;
+            tv.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    viewPager.setCurrentItem(finalI,true);
+                }
+            });
+            tabs.add(tv);
+            container.addView(tv);
         }
     }
 }
